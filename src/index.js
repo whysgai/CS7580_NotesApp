2fa206f24071e4fc3f2df8d212b1eba5a2ab7f21
Will Cohen <will.r.cohen@gmail.com>
2021-02-16 16:41:58 -0500

    A2 chore: Install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
