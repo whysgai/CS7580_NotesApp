a8ac03276e3f8f1273e9f90377b53d254be3c489
Will Cohen <will.r.cohen@gmail.com>
2021-02-16 16:36:47 -0500

    A2 feat: Add conditional functionality to chose proper note body

--- src/components/NoteShell.js
+++ src/components/NoteShell.js
@@ -1,21 +1,17 @@
 import {NOTE_TYPE} from '../data/data.js'
+import NoteText from './NoteText.js'
 
 const NoteShell = props =>
     <div className="col gy-4">
         <div className="card">
            {
                 props.type === NOTE_TYPE.TEXT &&
-                    <p>Text</p>
+                    <NoteText title={props.title} text={props.text} />
            }
            {
                 props.type === NOTE_TYPE.LINK &&
-                    <p>Link</p>
+                    <NoteURL url={props.url} text={props.text} />
            }
-            <div className="card-body">            
-                <h2 className="card-title">{props.title}</h2>
-                <h3 className="card-text">{props.species}</h3>
-                <p className="card-text">{props.age}</p>
-            </div>
         </div>
     </div>
 
--- src/views/App.js
+++ src/views/App.js
@@ -1,4 +1,10 @@
-const App = () =>
-    <p>Components go here</p>
+import data from '../data/data.js'
+
+
+const App = () => {
+    return (
+        <div className="container"></div>
+    )
+}
 
 export default App;
