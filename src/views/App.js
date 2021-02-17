221c5b7939282d1abf643c766fb6c4268bb97fc6
Will Cohen <will.r.cohen@gmail.com>
2021-02-17 17:10:04 -0500

    A2 feat: Add hook for project filter

--- src/views/App.js
+++ src/views/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import data from '../data/data.js';
 import Project from '../components/Project.js';
 import NoteList from '../components/NoteList.js';
@@ -6,8 +7,11 @@ import ProjectFilter from '../components/ProjectFilter.js';
 
 
 const App = () => {
+    const [visibleProjects, viewProject] = useState([]);
+
     return (
         <div className="container">
+            {console.log(visibleProjects)}
             <div className="row">
                 <div className="col-3">
                     <ProjectFilter projects={data}/>
