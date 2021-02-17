d85c0826388a68e1f8c145752afd7651414f4ff2
Will Cohen <will.r.cohen@gmail.com>
2021-02-17 14:17:27 -0500

    A2 feat: Add conditionality to note url display text

--- src/components/NoteURL.js
+++ src/components/NoteURL.js
@@ -1,6 +1,6 @@
 const NoteLink = props =>
     <div className="card-body">            
-        <a className="card-title" href={props.url}>{props.text}</a>
+        <a className="card-title" href={props.url}>{props.text != "" ? props.text : props.url}</a>
     </div>
 
 export default NoteLink;
