e9513f48a429a861865942109e22c23fbc998b2f
Will Cohen <will.r.cohen@gmail.com>
2021-02-22 12:09:22 -0500

    A2 feat: Implement mutally exclusive collapses on new text cards

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -4,23 +4,25 @@ import NewLinkNote from './NewLinkNote.js';
 const NewNote = props =>
     <div className="">            
         <button type="button" className="btn btn-primary col-12" type="button" data-bs-toggle="collapse" data-bs-target="#new-note-types" aria-expanded="false" aria-controls="new-note-types">New Note</button>
-        <div className="btn-group col-12 collapse" id="new-note-types">
-            <button type="button" className="btn btn-outline-dark"
-                data-bs-toggle="collapse" data-bs-target="#new-text-note" aria-expanded="false" aria-controls="new-text-note"
-            >
-                Text Note
-            </button>
-            <button type="button" className="btn btn-outline-dark"
-                data-bs-toggle="collapse" data-bs-target="#new-link-note" aria-expanded="false" aria-controls="new-link-note"
-            >
-                URL Note
-            </button>
-        </div>
-        <div className="collapse" id="new-text-note">
-            <NewTextNote projects={props.projects} newTextNote={props.newTextNote} setNewTextNote={props.setNewTextNote} addNewNote={props.addNewNote} newTextAlert={props.newTextAlert}/>
-        </div>
-        <div className="collapse" id="new-link-note">
-            <NewLinkNote projects={props.projects} newLinkNote={props.newLinkNote} setNewLinkNote={props.setNewLinkNote} addNewNote={props.addNewNote} newLinkAlert={props.newLinkAlert}/>
+        <div id="new-note-collapse-parent">
+            <div className="btn-group col-12 collapse" id="new-note-types">
+                <button type="button" className="btn btn-outline-dark"
+                    data-bs-toggle="collapse" data-bs-target="#new-text-note" aria-expanded="false" aria-controls="new-text-note"
+                >
+                    Text Note
+                </button>
+                <button type="button" className="btn btn-outline-dark"
+                    data-bs-toggle="collapse" data-bs-target="#new-link-note" aria-expanded="false" aria-controls="new-link-note"
+                >
+                    URL Note
+                </button>
+            </div>
+            <div className="collapse" id="new-text-note"  data-bs-parent="#new-note-collapse-parent" >
+                <NewTextNote projects={props.projects} newTextNote={props.newTextNote} setNewTextNote={props.setNewTextNote} addNewNote={props.addNewNote} newTextAlert={props.newTextAlert}/>
+            </div>
+            <div className="collapse" id="new-link-note"  data-bs-parent="#new-note-collapse-parent" >
+                <NewLinkNote projects={props.projects} newLinkNote={props.newLinkNote} setNewLinkNote={props.setNewLinkNote} addNewNote={props.addNewNote} newLinkAlert={props.newLinkAlert}/>
+            </div>
         </div>
     </div>
 
