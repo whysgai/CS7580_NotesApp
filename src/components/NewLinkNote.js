afd4c214336acf6469fc8c575c907b6de2143865
Will Cohen <whysgai@gmail.com>
2021-02-21 19:08:01 -0500

    A2 style: Associate labels with input elements

--- src/components/NewLinkNote.js
+++ src/components/NewLinkNote.js
@@ -4,19 +4,32 @@ import { NO_PROJECT } from '../data/data.js';
 const NewLinkNote = props =>
     <div className="card card-body">            
         <h3 className="card-title">New Note</h3>
-        <label>Project: </label>
-        <select id="new-link-project">
-            <option value={NO_PROJECT}>--No Project--</option>
-            {
-                Object.keys(props.projects).map((project, index) =>
-                    <option key={index}>{project}</option>                
-                )
-            } 
-        </select>
-        <label>URL:</label>
-        <input type="url" onChange={e => props.setNewLinkNote({...props.newLinkNote, url : e.target.value})} value={props.newLinkNote.url}></input>
-        <label>Text:</label>
-        <input type="text" onChange={e => props.setNewLinkNote({...props.newLinkNote, text : e.target.value})} value={props.newLinkNote.text}></input>
+        <label className="form-label">Project:
+            <select className="form-control">
+                <option value={NO_PROJECT}>--No Project--</option>
+                {
+                    Object.keys(props.projects).map((project, index) =>
+                        <option key={index}>{project}</option>                
+                    )
+                } 
+            </select>
+        </label>        
+        <label className="form-label">URL:
+            <input type="url" 
+                className="form-control"
+                onChange={e => props.setNewLinkNote({...props.newLinkNote, url : e.target.value})}
+                value={props.newLinkNote.url}
+            />
+        </label>
+        
+        <label className="forn-label">Text:
+        <input type="text"
+            className="form-control" 
+            onChange={e => props.setNewLinkNote({...props.newLinkNote, text : e.target.value})}
+            value={props.newLinkNote.text}
+        />
+        </label>
+        
         <button className="btn btn-primary col-12"
             onClick={() => {
                 props.addNewNote(document.getElementById("new-link-project").value, props.newLinkNote);
--- src/components/NewTextNote.js
+++ src/components/NewTextNote.js
@@ -5,19 +5,30 @@ import { NO_PROJECT } from '../data/data.js';
 const NewTextNote = props =>
     <div className="card card-body">            
         <h3 className="card-title">New Note</h3>
-        <label>Project: </label>
-        <select id="new-text-project">
-            <option value={NO_PROJECT}>--No Project--</option>
-            {
-                Object.keys(props.projects).map((project, index) =>
-                    <option key={index}>{project}</option>                
-                )
-            }                      
-        </select>
-        <label>Title:</label>
-        <input type="text" onChange={e => props.setNewTextNote({...props.newTextNote, title : e.target.value})} value={props.newTextNote.title}></input>
-        <label>Text:</label>
-        <textarea type="textarea" onChange={e => props.setNewTextNote({...props.newTextNote, text : e.target.value})} value={props.newTextNote.text}></textarea>
+        <label className="form-label">Project: 
+            <select className="form-control">
+                <option value={NO_PROJECT}>--No Project--</option>
+                {
+                    Object.keys(props.projects).map((project, index) =>
+                        <option key={index}>{project}</option>                
+                    )
+                }                      
+            </select>
+        </label>
+        <label className="form-label">Title:
+            <input type="text"
+                className="form-control"
+                onChange={e => props.setNewTextNote({...props.newTextNote, title : e.target.value})}
+                value={props.newTextNote.title}
+                id="new-text-title"
+            />
+        </label>
+        <label className="form-label">Text:
+            <textarea type="textarea" 
+                className="form-control"
+                onChange={e => props.setNewTextNote({...props.newTextNote, text : e.target.value})} value={props.newTextNote.text}
+            />
+        </label>
         <button className="btn btn-primary col-12" 
             onClick={() => {
                 props.addNewNote(document.getElementById("new-text-project").value, props.newTextNote);
